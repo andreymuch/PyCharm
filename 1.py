ffd97ce29694edf0d77de563bdc864c1d2fb02e9
dmitry <dimats2350@gmail.com>
2022-04-16 15:54:17 +0300

    dmitry commit

--- 1.py
+++ 1.py
@@ -1,26 +1,45 @@
 import pickle
+
+
 class Kab:
+    """
+    data base
+    """
     phone = None
     num = None
     name = None
-    surn = None
+    surname = None
+
+
+# read new dates for our database
 f = open("students.txt", 'r', encoding='utf-8')
+# page in database
 St = []
+# list of people in this room
 b = []
 while True:
+    # read new dates
     x = f.readline()
     if not x:
         break
+    # init
     x = x.split(' ', 2)
     st = Kab()
     st.phone = x[0]
     st.num = x[1]
     b = x[2].split(" ")
-    st.surn = [b[0], b[2]]
+    st.surname = [b[0], b[2]]
     st.name = [b[1], b[3]]
+
+    # append ordered dates to St
     St.append(st)
-    print(st.phone, st.num, st.name, st.surn)
-fout = open('students.dat', 'wb')
-pickle.dump(St, fout)
-fout.close()
+    print(st.phone, st.num, st.name, st.surname)
+
+# entry to database a new page
+file_out = open('students.dat', 'wb')
+pickle.dump(St, file_out)
+file_out.close()
 f.close()
+
+if __name__ == "__main__":
+    pass
--- 2.py
+++ 2.py
@@ -1,63 +1,82 @@
 import pickle
+
+
 class Kab:
-    phone=None
+    phone = None
     num = None
     name = None
-    surn = None
+    surname = None
+
+
+# open database to read dates from it
 f = open('students.dat', 'rb')
 Ka = pickle.load(f)
 f.close()
+
 while True:
-    print()
-    print("Введите число")
-    print("1-Вывод по номеру телефона")
-    print("2-Вывод по номеру кабинета")
-    print("3-Вывод по Фамилии")
-    print("4-Дабл эл")
-    print("5-Вывод БД")
-    print("6-Завершение прогграмы")
-    n=int(input())
+    # database command processing
+    try:
+        n = int(input("""
+        ______________________________________
+        _ Enter number:                      _
+        _     1: Вывод по номеру телефона    _
+        _     2: Вывод по номеру кабинета    _
+        _     3: Вывод по Фамилии            _
+        _     4: Дабл эл                     _
+        _     5: Вывод БД                    _
+        _     6: Завершение прогграмы        _
+        ______________________________________
+        """))
+    except TypeError:
+        print("This command isn't possible!!!! "
+              "Try again!!")
+        break
+
     if n == 1:
         ph = input()
         for i in range(len(Ka)):
             if ph == Ka[i].phone:
-                print(Ka[i].num, Ka[i].name[i], Ka[i].surn[i], Ka[i].name[i+1], Ka[i].surn[i+1])
+                print(Ka[i].num, end=": ")
+                for fio in range(len(Ka[i].name)):
+                    if fio != len(Ka[i].name) - 1:
+                        print(Ka[i].name[fio], Ka[i].surname[fio], end=", ")
+                    else:
+                        print(Ka[i].name[fio], Ka[i].surname[fio])
     elif n == 2:
         nu = input()
         for i in Ka:
             if nu == i.num:
                 print(i.phone)
     elif n == 3:
-        print("Введите Фамилию")
-        sur = input()
+        sur = input("Введите Фамилию").strip()
         for i in range(len(Ka)):
-            for j in range(len(Ka[i].surn)):
-                if sur == Ka[i].surn[j]:
+            for j in range(len(Ka[i].surname)):
+                if sur == Ka[i].surname[j]:
                     print(Ka[i+j].phone, Ka[i+j].num)
     elif n == 4:
         f = open('students.dat', 'ab')
         st = Kab()
-        print("Введите номер")
-        st.phone = input()
-        print("Введите номер кабинета")
-        st.num = input()
-        print("Введите кол-во новых людей")
-        kol = int(input())
+        st.phone = input("Введите номер").strip()
+        st.num = input("Введите номер кабинета").strip()
+        kol = int(input("Введите кол-во новых людей").strip())
         st.name = []
-        st.surn = []
+        st.surname = []
+
         for i in range(kol):
-            print("Введите человека(ФИ через пробел)")
-            b = list(input().split())
+            b = list(input("Введите человека(ФИ через пробел)").split())
             st.name.append(b[0])
-            st.surn.append(b[1])
+            st.surname.append(b[1])
         Ka.append(st)
         pickle.dump(Ka, f)
         f.close()
     elif n == 5:
         for i in Ka:
-            print(i.phone, i.num, *i.name, *i.surn)
+            print(i.phone, i.num, *i.name, *i.surname)
     elif n == 6:
         break
     else:
         print('Введите снова')
         pass
+
+if __name__ == "__main__":
+    pass
